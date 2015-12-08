9d851526ccb9c119edbc01e73c9ebc49245a5fef
Jakub Syty <jakub.nekro@gmail.com>
2015-12-08 15:33:24 +0100

    Speeches and interpolations lists

--- AplikacjaParlamentIOS/Controllers/PoselInterpelationsController.cs
+++ AplikacjaParlamentIOS/Controllers/PoselInterpelationsController.cs
@@ -2,13 +2,53 @@ using Foundation;
 using System;
 using System.CodeDom.Compiler;
 using UIKit;
+using AplikacjaParlamentShared.Repositories;
+using AplikacjaParlamentShared.Api;
+using AplikacjaParlamentShared.Models;
+using System.Collections.Generic;
 
 namespace AplikacjaParlamentIOS
 {
-	partial class PoselInterpelationsController : UIViewController
+	public partial class PoselInterpelationsController : BaseController
 	{
+		LoadingOverlay loadingOverlay;
+		UITableView TableView;
+
 		public PoselInterpelationsController (IntPtr handle) : base (handle)
 		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			TableView = new UITableView () {
+				Frame = new CoreGraphics.CGRect (0, 65, View.Bounds.Width, View.Bounds.Height - 115)
+			};
+			View.AddSubviews (new UIView[] { TableView });
+			EdgesForExtendedLayout = UIRectEdge.None;
+			var bounds = UIScreen.MainScreen.Bounds;
+			loadingOverlay = new LoadingOverlay (bounds);
+			View.Add (loadingOverlay);
+			GetData ();
+		}
+
+		async private void GetData()
+		{
+
+			IPeopleRepository repository = PeopleRepository.Instance;
+			try {
+				var list = await repository.GetPoselInterpellations ((ParentViewController as PoselController).PoselID);
+
+				TableView.Source = new PoselInterpellationsTableSource(list, this);
+				TableView.ReloadData();
+			} catch (ApiRequestException ex){
+				DisplayError(ex.Message);
+				System.Diagnostics.Debug.WriteLine (ex.Message);
+			} catch (Exception exc){
+				System.Diagnostics.Debug.WriteLine (exc.Message);
+			} finally {
+				loadingOverlay.Hide();
+			}
 		}
 	}
 }
